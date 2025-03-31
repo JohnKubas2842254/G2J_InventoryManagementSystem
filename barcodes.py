cf84ebe6f3d523d8e60c79b6c07192ced697b8f0
JohnKubas2842254 <j.kubas47@vikes.csuohio.edu>
2025-03-31 13:45:38 -0400

    Create barcodes.py

---
+++ barcodes.py
@@ -0,0 +1,12 @@
+barcodes = {
+"037000941514": "Charmin Ultra-Strong Toilet Paper",
+"715141627039": "Large Brown Eggs",
+"715141328615": "Jumbo White Eggs",
+"696551750930": "A & W Root Beer Soda 12 Pack",
+"074590516654": "Quaker Instant Oatmeal",
+"041457878026": "Honey Nut Cheerios",
+"030100062134": "Rice Krispies Treats Bars",
+"077567019905": "Oreo Cookies Cream Filled Double Stuf 10.25 Oz",
+"722252369840": "Clif Bars Peanut Butter",
+"785357012059": "Peet's Coffee Major Dickason's K-Cup"
+}
